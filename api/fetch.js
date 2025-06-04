472d37dec23a7f4ba0b8c2ffc28401c00a66b55d
RamDiv <142648607+Bintooot@users.noreply.github.com>
2025-06-04 13:47:39 +0800

    adding svg assests

---
+++ api/fetch.js
@@ -0,0 +1,53 @@
+async function fetchGithubUser() {
+  const usernameInput = document.getElementById("search");
+  const result = document.getElementById("error");
+
+  const username = usernameInput.value.trim();
+
+  if (!username) {
+    result.innerHTML = "Please enter username";
+    return;
+  }
+
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    if (!response.ok) {
+      alert("User not found");
+      return;
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    // Personal Info //
+    document.getElementById("avatar").src = data.avatar_url || "No Avatar";
+    document.getElementById("name").textContent = data.login || "No name";
+    document.getElementById("username").textContent =
+      data.name || "No usernmae";
+    document.getElementById("bio").textContent = data.bio || "No bio";
+    document.getElementById("joined").textContent = `Joined: ${new Date(
+      data.created_at
+    ).getFullYear()}`;
+
+    // Total Count of Repositories, Following, Folllowers //
+    document.getElementById("repos").textContent = data.public_repos || "0";
+    document.getElementById("following").textContent = data.following || "0";
+    document.getElementById("followers").textContent = data.followers || "0";
+
+    // External Links //
+    document.getElementById("github-link").href = data.html_url;
+    document.getElementById("github-link").textContent = "GitHub";
+
+    document.getElementById("blog-link").href = data.blog || "#";
+    document.getElementById("blog-link").textContent = data.blog
+      ? "Blog"
+      : "No blog";
+
+    document.getElementById("email-link").href = "#";
+    document.getElementById("email-link").textContent = data.mail || "No Email";
+  } catch (error) {
+    console.error("Fetch error:", error);
+    alert("An error occurred while fetching user data.");
+  }
+}
